66e47a7d0d78fa1a63832238c69535f0c0e266d9
Simon Okelo <oksimonvich@gmail.com>
2024-07-17 14:58:19 +0300

    Changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { RegistrationForm } from "@/components/Registration/RegistrationForm";
 
 export default function Home() {
   return (
     <main className="grid place-items-center h-screen p-24">
+      <RegistrationForm />
+
       <Button>Contribute</Button>
+
     </main>
   );
 }
