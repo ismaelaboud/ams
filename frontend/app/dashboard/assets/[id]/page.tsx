1661dcecfcb368beb7790ae549d5fa984b441b3a
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-07-17 17:38:10 +0300

    update dashboard and add some dashboard pages

--- frontend/app/dashboard/assets/[id]/page.tsx
+++ frontend/app/dashboard/assets/[id]/page.tsx
@@ -1,31 +1,30 @@
 import Link from "next/link";
-import { Plus } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import ProfileDetails from "@/components/dashboard/profile";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
-export default async function Profile() {
+export default async function ViewAsset() {
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4  p-4 pt-6 md:p-8">
         <div className="flex items-start justify-between">
           <div>
-            <h2 className="text-3xl font-bold tracking-tight">Profile</h2>
+            <h2 className="text-3xl font-bold tracking-tight">View asset</h2>
             <p className="text-sm text-muted-foreground tracking-tight">
-              View your profile or add new asset
+              View asset details here
             </p>
           </div>
           <Link
-            href={"/dashboard/assets/new"}
+            href={"/dashboard/assets"}
             className={cn(buttonVariants({ variant: "default" }))}
           >
-            <Plus className="mr-2 h-4 w-4" /> New Asset
+            View Assets <ChevronRight className="ml-2 h-4 w-4" />
           </Link>
         </div>
         <Separator />
-        <ProfileDetails />
+        <p>Asset details goes here</p>
       </div>
     </ScrollArea>
   );
