7c2ec9a79262383df8fc8f1f993adca769e073be
cyber-security <ngemuantony@gmail.com>
2024-07-19 18:21:21 +0300

    Reviewed the code recieved from other collaborators

--- backend/assets/serializers.py
+++ backend/assets/serializers.py
@@ -2,6 +2,8 @@
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from .models import CustomUser, Category, Tag, Asset
+from assets.serializers import CategorySerializer, TagSerializer, AssetSerializer, RegisterSerializer, LoginSerializer
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
