560eff7c59a0a157ed90ba5115e03be272ea2bb1
Landmine254 <makosabisho@gmail.com>
2024-07-22 11:44:39 +0000

    added comments on models.py and seializers

--- backend/assets/models.py
+++ backend/assets/models.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
+# Custom user model extending AbstractUser
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True, max_length=50)
-    firstName = models.CharField(max_length=30)
-    lastName = models.CharField(max_length=30)
+    email = models.EmailField(unique=True, max_length=50)  # Unique email field
+    firstName = models.CharField(max_length=30)  # First name field
+    lastName = models.CharField(max_length=30)  # Last name field
     
+    # Many-to-many field for user groups
     groups = models.ManyToManyField(
         'auth.Group',
         related_name='customUserGroups',
@@ -16,6 +18,7 @@ class CustomUser(AbstractUser):
         help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
     )
     
+    # Many-to-many field for user permissions
     user_permissions = models.ManyToManyField(
         'auth.Permission',
         related_name='customUserPermissions',
@@ -25,23 +28,25 @@ class CustomUser(AbstractUser):
     )
 
     def __str__(self):
-        return self.username
+        return self.username  # String representation of the user
 
 
+# Model representing a department
 class Department(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    id = models.AutoField(primary_key=True)  # Auto-incrementing primary key
+    name = models.CharField(max_length=50)  # Name of the department
 
     def __str__(self):
-        return self.name
+        return self.name  # String representation of the department
 
     class Meta:
-        db_table = 'departments'
+        db_table = 'departments'  # Custom database table name
 
 
+# Signal receiver to create default departments after migrations
 @receiver(post_migrate)
 def create_default_departments(sender, **kwargs):
-    if sender.name == 'assets':
+    if sender.name == 'assets':  # Check if the sender app is 'assets'
         default_departments = [
             'Technology',
             'Communication',
@@ -53,10 +58,12 @@ def create_default_departments(sender, **kwargs):
             'Entrepreneurship',
             'Engineering',
         ]
+        # Create each default department if it doesn't exist
         for department_name in default_departments:
             Department.objects.get_or_create(name=department_name)
 
 
+# Model representing a user profile
 class Profile(models.Model):
     USER_ROLE = 'USER'
     ADMIN_ROLE = 'ADMIN'
@@ -66,51 +73,57 @@ class Profile(models.Model):
         (ADMIN_ROLE, 'Admin'),
     ]
 
-    id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    role = models.CharField(max_length=50, choices=ROLE_CHOICES, default=USER_ROLE)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)  # Auto-incrementing primary key
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)  # One-to-one relationship with CustomUser
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES, default=USER_ROLE)  # Role field with choices
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)  # Foreign key to Department
 
     def __str__(self):
-        return self.user.username
+        return self.user.username  # String representation of the profile
 
     class Meta:
-        db_table = 'profiles'
+        db_table = 'profiles'  # Custom database table name
 
+
+# Model representing a category
 class Category(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    id = models.AutoField(primary_key=True)  # Auto-incrementing primary key
+    name = models.CharField(max_length=50)  # Name of the category
 
     def __str__(self):
-        return self.name
+        return self.name  # String representation of the category
 
     class Meta:
-        db_table = 'categories'  # Table name for Category model
+        db_table = 'categories'  # Custom database table name
 
 
+# Signal receiver to create default categories after migrations
 @receiver(post_migrate)
 def create_default_categories(sender, **kwargs):
-    if sender.name == 'assets':
+    if sender.name == 'assets':  # Check if the sender app is 'assets'
         default_categories = [
             'Furnitures',
             'Electronics',
             'Office Supplies',
         ]
+        # Create each default category if it doesn't exist
         for category_name in default_categories:
             Category.objects.get_or_create(name=category_name)
 
 
+# Model representing a tag
 class Tag(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    id = models.AutoField(primary_key=True)  # Auto-incrementing primary key
+    name = models.CharField(max_length=50)  # Name of the tag
 
     def __str__(self):
-        return self.name
+        return self.name  # String representation of the tag
 
     class Meta:
-        db_table = 'tags'        
+        db_table = 'tags'  # Custom database table name
 
 
+# Model representing an asset
 class Asset(models.Model):
     STATUS_CHOICES = [
         ('Available', 'Available'),
@@ -120,47 +133,46 @@ class Asset(models.Model):
         ('Archived', 'Archived'),
     ]
 
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    assetType = models.CharField(max_length=50)
-    description = models.TextField()
-    serialNumber = models.CharField(max_length=255, unique=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    assignedDepartment = models.ForeignKey(Department, on_delete=models.CASCADE)
-    dateRecorded = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Available')
+    id = models.AutoField(primary_key=True)  # Auto-incrementing primary key
+    name = models.CharField(max_length=50)  # Name of the asset
+    assetType = models.CharField(max_length=50)  # Type of the asset
+    description = models.TextField()  # Description of the asset
+    serialNumber = models.CharField(max_length=255, unique=True)  # Unique serial number
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # Foreign key to Category
+    assignedDepartment = models.ForeignKey(Department, on_delete=models.CASCADE)  # Foreign key to Department
+    dateRecorded = models.DateTimeField(auto_now_add=True)  # Date the asset was recorded
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Available')  # Status of the asset
 
     def __str__(self):
-        return self.name
+        return self.name  # String representation of the asset
 
     class Meta:
-        db_table = 'assets'
+        db_table = 'assets'  # Custom database table name
 
 
-
+# Model representing an asset tag relationship
 class AssetTag(models.Model):
-    id = models.AutoField(primary_key=True)
-    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)  # Auto-incrementing primary key
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)  # Foreign key to Asset
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)  # Foreign key to Tag
 
     class Meta:
-        db_table = 'asset_tags'
-        unique_together = ('asset', 'tag')
+        db_table = 'asset_tags'  # Custom database table name
+        unique_together = ('asset', 'tag')  # Unique constraint for asset and tag
 
 
-
+# Model representing an asset assignment
 class AssetAssignment(models.Model):
-    id = models.AutoField(primary_key=True)
-    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    assignedTo = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    assignedDepartment = models.ForeignKey(Department, on_delete=models.CASCADE)
-    dateAssigned = models.DateField(auto_now_add=True)
-    returnDate = models.DateField(null=True, blank=True)
+    id = models.AutoField(primary_key=True)  # Auto-incrementing primary key
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)  # Foreign key to Asset
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  # Foreign key to CustomUser
+    assignedTo = models.ForeignKey(Profile, on_delete=models.CASCADE)  # Foreign key to Profile
+    assignedDepartment = models.ForeignKey(Department, on_delete=models.CASCADE)  # Foreign key to Department
+    dateAssigned = models.DateField(auto_now_add=True)  # Date the asset was assigned
+    returnDate = models.DateField(null=True, blank=True)  # Optional return date
 
     def __str__(self):
-        return f"{self.asset.name} assigned to {self.assignedTo.user.username}"
+        return f"{self.asset.name} assigned to {self.assignedTo.user.username}"  # String representation of the assignment
 
     class Meta:
-        db_table = 'assetAssignment'  # Table name for AssetAssignment model
-
+        db_table = 'assetAssignment'  # Custom database table name
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,4 +1,11 @@
-# backend/settings.py
+"""
+Settings module for the Django project.
+
+This file contains the default configuration for the Django application, including settings
+for installed applications, middleware, templates, database configuration, password validation,
+internationalization, static files, and more. It also includes configurations for third-party 
+applications like Django REST framework, JWT authentication, and email backend settings.
+"""
 
 import os
 from pathlib import Path
@@ -8,7 +15,10 @@ from datetime import timedelta
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-sv8hm9fmt3$$_pdjz7xl3)dclejl(g358sl7o+jk-0f_!r&my+'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # Update ALLOWED_HOSTS with the domains you want to allow
@@ -16,29 +26,29 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'corsheaders',
-    'django_filters',
-    'rest_framework_simplejwt.token_blacklist',
-    'assets',
-    'django_rest_passwordreset',
+    'django.contrib.admin',  # Admin site
+    'django.contrib.auth',  # Authentication framework
+    'django.contrib.contenttypes',  # Content types framework
+    'django.contrib.sessions',  # Session framework
+    'django.contrib.messages',  # Messaging framework
+    'django.contrib.staticfiles',  # Static files handling
+    'rest_framework',  # Django REST framework
+    'corsheaders',  # CORS headers handling
+    'django_filters',  # Filtering for Django REST framework
+    'rest_framework_simplejwt.token_blacklist',  # JWT token blacklist
+    'assets',  # Custom application for managing assets
+    'django_rest_passwordreset',  # Password reset functionality
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # CORS middleware
+    'django.middleware.security.SecurityMiddleware',  # Security middleware
+    'django.contrib.sessions.middleware.SessionMiddleware',  # Session middleware
+    'django.middleware.common.CommonMiddleware',  # Common middleware
+    'django.middleware.csrf.CsrfViewMiddleware',  # CSRF protection middleware
+    'django.contrib.auth.middleware.AuthenticationMiddleware',  # Authentication middleware
+    'django.contrib.messages.middleware.MessageMiddleware',  # Messaging middleware
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',  # Clickjacking protection middleware
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -46,14 +56,14 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [],  # Directories to search for templates
+        'APP_DIRS': True,  # Enable template loading from application directories
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',  # Debug context processor
+                'django.template.context_processors.request',  # Request context processor
+                'django.contrib.auth.context_processors.auth',  # Authentication context processor
+                'django.contrib.messages.context_processors.messages',  # Messages context processor
             ],
         },
     },
@@ -61,110 +71,99 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# Database
+# Database configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',  # SQLite database engine
+        'NAME': BASE_DIR / 'db.sqlite3',  # Database file path
     }
 }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},  # Similarity validator
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},  # Minimum length validator
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},  # Common password validator
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},  # Numeric password validator
 ]
 
-# Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
+# Internationalization settings
+LANGUAGE_CODE = 'en-us'  # Language code
+TIME_ZONE = 'UTC'  # Time zone
+USE_I18N = True  # Enable internationalization
+USE_TZ = True  # Enable timezone support
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images) settings
+STATIC_URL = 'static/'  # URL to access static files
 
 # Default primary key field type
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'  # Default primary key field type
 
 # Custom user model
-AUTH_USER_MODEL = 'assets.CustomUser'
+AUTH_USER_MODEL = 'assets.CustomUser'  # Custom user model
 
-# ============================== CONFIGURING COR HEADERS TO RESTRICT HOST ACCESS =================
+# Configuring CORS headers to restrict host access
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5000",
     "http://localhost:8000",
     # "http://95cc-197-237-236-78.ngrok-free.app",  # Add your domain here
 ]
 
-
-# =============================== JWF SETTINGS CONFIGURATION =======================================
+# JWT settings configuration
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  # JWT authentication class
     ),
 }
 
-# JWT settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
-    'UPDATE_LAST_LOGIN': False,
-    'ALGORITHM': 'HS256',
-    'SIGNING_KEY': SECRET_KEY,
-    'VERIFYING_KEY': None,
-    'AUDIENCE': None,
-    'ISSUER': None,
-    'JSON_ENCODER': None,
-    'JWK_URL': None,
-    'LEEWAY': 0,
-    'AUTH_HEADER_TYPES': ('Bearer',),
-    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
-    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',
-    'JTI_CLAIM': 'jti',
-    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=120),
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=30),
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=120),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),  # Access token lifetime
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),  # Refresh token lifetime
+    'ROTATE_REFRESH_TOKENS': False,  # Rotate refresh tokens setting
+    'BLACKLIST_AFTER_ROTATION': True,  # Blacklist after rotation setting
+    'UPDATE_LAST_LOGIN': False,  # Update last login setting
+    'ALGORITHM': 'HS256',  # Algorithm for signing tokens
+    'SIGNING_KEY': SECRET_KEY,  # Signing key for tokens
+    'VERIFYING_KEY': None,  # Verifying key for tokens
+    'AUDIENCE': None,  # Audience for tokens
+    'ISSUER': None,  # Issuer for tokens
+    'JSON_ENCODER': None,  # JSON encoder for tokens
+    'JWK_URL': None,  # JWK URL for tokens
+    'LEEWAY': 0,  # Leeway for token expiration
+    'AUTH_HEADER_TYPES': ('Bearer',),  # Auth header types
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',  # Auth header name
+    'USER_ID_FIELD': 'id',  # User ID field
+    'USER_ID_CLAIM': 'user_id',  # User ID claim
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',  # User authentication rule
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),  # Auth token classes
+    'TOKEN_TYPE_CLAIM': 'token_type',  # Token type claim
+    'JTI_CLAIM': 'jti',  # JTI claim
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',  # Sliding token refresh expiration claim
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=120),  # Sliding token lifetime
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),  # Sliding token refresh lifetime
 }
 
+# Email backend settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Email backend
+EMAIL_HOST = 'smtp.gmail.com'  # Email host
+EMAIL_PORT = 587  # Email port
+EMAIL_USE_TLS = True  # Use TLS for email
+EMAIL_HOST_USER = 'developerantony98@gmail.com'  # Email host user
+EMAIL_HOST_PASSWORD = 'ejid tsyb acow emcs'  # Email host password
+DEFAULT_FROM_EMAIL = 'noreply@gmail.com'  # Default from email
 
-
-# ============================ EMAILS AND PASSWORD RESET FRAMWORK SETTINGS =============================
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'developerantony98@gmail.com'
-EMAIL_HOST_PASSWORD = 'ejid tsyb acow emcs'
-DEFAULT_FROM_EMAIL = 'noreply@gmail.com'
-
-# =========================== CONFIGURING SEND EMAIL TEMPLATES ===========================================
-
+# Configuring send email templates
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Directories to search for templates
+        'APP_DIRS': True,  # Enable template loading from application directories
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',  # Debug context processor
+                'django.template.context_processors.request',  # Request context processor
+                'django.contrib.auth.context_processors.auth',  # Authentication context processor
+                'django.contrib.messages.context_processors.messages',  # Messages context processor
             ],
         },
     },
